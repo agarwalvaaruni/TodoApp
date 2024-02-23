eb8f764f46916a4c4365be95ae2e0f43f0cdfc34
vaaruniagarwal <vaaruni.agarwal@iesoftek.com>
2024-02-23 23:28:43 +0530

    Multer no change in file name

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ const multer = require("multer");
 
 const app = express();
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-PostMan.jpg");
-    },
-  }),
-});
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, "uploads");
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, file.fieldname + "-PostMan.jpg");
+//     },
+//   }),
+// });
 
-app.post("/stats", upload.single("uploaded_file"), (req, res)=>{
-  res.status(200).send("File uploaded successfully!");
+// app.post("/stats", upload.single("uploaded_file"), (req, res)=>{
+//   res.status(200).send("File uploaded successfully!");
+// });
+
+const upload = multer({ dest: 'uploads'})   //if do not want to change filename
+app.post('/stats', upload.single('uploaded_file'), function (req, res) {
+   res.send("Uploaded");
 });
 
 app.listen(5000);
